10634bae51417567f0ccb52ab953dd76cc13d14d
juicyka <qzzhu1@student.unimelb.edu.au>
2022-08-30 17:17:03 +1000

    Set up file structure and implement some part of Login Page

---
+++ game-101/src/components/login/GameLogo.js
@@ -0,0 +1,11 @@
+import Logo from "../../logo.svg";
+
+const GameLogo = () => {
+  return (
+    <div>
+      <img src={Logo} alt="Game Logo" />
+    </div>
+  );
+};
+
+export default GameLogo;
---
+++ game-101/src/components/login/LoginBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginBar = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  );
+};
+
+export default LoginBar;
