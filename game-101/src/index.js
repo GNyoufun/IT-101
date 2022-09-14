1d1278f48e6e1a2278953042f5d16fddcc42b321
juicyka <qzzhu1@student.unimelb.edu.au>
2022-09-14 18:04:24 +1000

    Implement BackButton

--- game-101/src/index.js
+++ game-101/src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import reportWebVitals from "./reportWebVitals";
-import { GameHistory } from "./pages";
+import { GameHistoryRecord } from "./pages";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GameHistory />
+    <GameHistoryRecord />
   </React.StrictMode>
 );
 
