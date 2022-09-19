6856f57753697923fa170a005d419a2fb0ba6100
juicyka <qzzhu1@student.unimelb.edu.au>
2022-09-19 12:52:34 +1000

    Make Contents in GameHistory scrollable

--- game-101/src/index.js
+++ game-101/src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import reportWebVitals from "./reportWebVitals";
-import { GameHistoryRecord } from "./pages";
+import { Setting } from "./pages";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GameHistoryRecord />
+    <Setting />
   </React.StrictMode>
 );
 
