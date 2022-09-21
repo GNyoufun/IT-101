971f3cf69396dde1b772516649b75818224b95c3
pH7uang <pH7uang@gmail.com>
2022-09-21 17:21:11 +1000

    changing raid record input/output structure

--- game-101/src/components/addNewRecord/SelectTeammates.js
+++ game-101/src/components/addNewRecord/SelectTeammates.js
@@ -8,28 +8,27 @@ export default function SelectTeammates(props) {
   const handleChange = (e, newValues) => {
     props.setInputs({...props.inputs, team: newValues});
   }
-  
+
   const teammates = [
-    { in_game_id: 'Brendan'},
-    { in_game_id: 'Ella'},
-    { in_game_id: 'Ishaann'},
-    { in_game_id: 'Jessica' },
-    { in_game_id: 'Patrick'},
+    { in_game_id: 'Brendan', level: 3},
+    { in_game_id: 'Ella', level: 3},
+    { in_game_id: 'Ishaann', level: 3},
+    { in_game_id: 'Jessica', level: 3},
+    { in_game_id: 'Patrick', level: 3},
   ];
 
   return (
       <Autocomplete
         multiple
-        id="tags-filled"
-        options={teammates.map((option) => option.in_game_id)}
-        freeSolo
+        id="team"
+        //freeSolo
+        options={teammates.map((option) => ('lv.' + option.level + ' ' + option.in_game_id))}
         onChange={handleChange}
         renderTags={(value, getTagProps) =>
           value.map((option, index) => (
-            <Chip label={option} {...getTagProps({ index })} />
+            <Chip label={option} {...getTagProps({index})} />
           ))
         }
-
         renderInput={(params) => (
           <TextField
             {...params}
