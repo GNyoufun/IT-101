830fca6bdb2fa86e55a38b82208fc139c60fce57
juicyka <qzzhu1@student.unimelb.edu.au>
2022-09-27 23:58:27 +1000

    Change addNew layout

--- game-101/src/components/addNew/addNew.js
+++ game-101/src/components/addNew/addNew.js
@@ -1,65 +1,62 @@
 import * as React from "react";
 
-import {
-  Button,
-  FormControl,
-  FormControlLabel,
-  FormHelperText,
-  FormLabel,
-  Radio,
-  RadioGroup,
-} from "@mui/material";
+import { Link } from "react-router-dom";
 
-export default function AddNew() {
-  const [value, setValue] = React.useState("");
-  const [error, setError] = React.useState(false);
-  const [helperText, setHelperText] = React.useState("Choose an option");
+import AddIcon from "@mui/icons-material/Add";
+import { Box, Container, Fab, Grid, Paper, Typography } from "@mui/material";
 
-  const handleRadioChange = (event) => {
-    setValue(event.target.value);
-    setHelperText(" ");
-    setError(false);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (value === "add-new-game") {
-      setHelperText("add-new-game");
-    } else if (value === "add-new-record") {
-      setHelperText("add-new-record");
-    } else {
-      setHelperText("Please select an option.");
-    }
-  };
-
+export default function DisableElevation() {
   return (
-    <form onSubmit={handleSubmit}>
-      <FormControl sx={{ m: 3 }} error={error} variant="standard">
-        <FormLabel id="add-new">
-          Please choose what are you going to add...
-        </FormLabel>
-        <RadioGroup
-          aria-labelledby="add-new"
-          name="add-new"
-          value={value}
-          onChange={handleRadioChange}
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        flexGrow: 2,
+        height: "100vh",
+        overflow: "auto",
+      }}
+    >
+      <Container maxWidth="sm" sx={{ mt: 20 }}>
+        <Paper
+          sx={{
+            p: 4,
+          }}
         >
-          <FormControlLabel
-            value="add-new-game"
-            control={<Radio />}
-            label="Add New Game"
-          />
-          <FormControlLabel
-            value="add-new-record"
-            control={<Radio />}
-            label="Add New Record"
-          />
-        </RadioGroup>
-        <FormHelperText>{helperText}</FormHelperText>
-        <Button type="submit" variant="contained">
-          Select
-        </Button>
-      </FormControl>
-    </form>
+          <Grid
+            container
+            sx={{ justifyContent: "center", flexDirection: "column" }}
+            alignItems="center"
+          >
+            <Typography variant="h5">Add New Game</Typography>
+            <Fab size="small" component={Link} to="/add-game">
+              <AddIcon />
+            </Fab>
+          </Grid>
+        </Paper>
+      </Container>
+      <Container maxWidth="sm" sx={{ mt: 20 }}>
+        <Paper
+          sx={{
+            p: 4,
+          }}
+        >
+          <Grid
+            container
+            sx={{ justifyContent: "center", flexDirection: "column" }}
+            alignItems="center"
+          >
+            <Typography variant="h5">Add New Record</Typography>
+            <Fab
+              color="secondary"
+              size="small"
+              component={Link}
+              to="/add-record"
+            >
+              <AddIcon />
+            </Fab>
+          </Grid>
+        </Paper>
+      </Container>
+    </Box>
   );
 }
