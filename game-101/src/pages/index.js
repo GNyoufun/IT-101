3555ee41d63a08a20199f2187d766f5274ab5d84
juicyka <qzzhu1@student.unimelb.edu.au>
2022-10-13 15:42:57 +1100

    delete unused page

--- game-101/src/pages/index.js
+++ game-101/src/pages/index.js
@@ -1,6 +1,5 @@
-import GameHistoryRecord from "./GameHistoryRecrod";
 import MainPage from "./MainPage";
 import Page404 from "./Page404";
 import SignInUp from "./SignInUp";
 
-export { GameHistoryRecord, MainPage, Page404, SignInUp };
+export { MainPage, Page404, SignInUp };
