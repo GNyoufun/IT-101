967b5e7adb3d64d1d8e2da1d0c65a92f2539ec55
Vauetrr <izag0502@gmail.com>
2022-10-14 21:29:34 +1100

    added teammates request

--- game-api/teammates.js
+++ game-api/teammates.js
@@ -1,11 +1,7 @@
 const ObjectId = require('mongodb').ObjectId;
 const {
     retrieveCollection,
-    updateCollection,
-    deleteCollection,
-    updateUserToken,
-    insertUser,
-    retrieveUserById,
+    extractTeam
 } = require('./databaseSrc/mongooseFunc.js');
 const {
     review
@@ -16,6 +12,27 @@ const {
  */
 module.exports = function (app) {
     app.get('/users/:user_id/teammates/:game', async (req, res, next) => {
-        res.send('get user id game');
+        console.log('Starting GET request /users/%s/teammates/%s', req.params.user_id, req.params.game);
+        try {
+            const id = ObjectId(req.params.user_id);
+            const game = req.params.game;
+
+            const result = await retrieveCollection(review, { UserId: id, Title: game }, { });
+            if (result.length === 0) {
+                // not found user id
+                res.sendStatus(404);
+                console.log('Failed GET request /users/%s/teammates/%s, 404', req.params.user_id, req.params.game);
+            } else {
+                // success
+                res.status(200);
+                res.json(extractTeam(result));
+                console.log('Successful GET request /users/%s/teammates/%s', req.params.user_id, req.params.game);
+            }
+        } catch (err) {
+            // invalid (not found) user id
+            res.sendStatus(404);
+            console.error(err);
+            console.log('Failed GET request /users/%s/teammates/%s, 404', req.params.user_id, req.params.game);
+        }
     });
 };
