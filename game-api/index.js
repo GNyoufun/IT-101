78b9b218ff857e1dc044c712488b33d11ca2f615
yunhl <yunhl@student.unimelb.edu.au>
2022-10-19 17:07:43 +1100

    serving the index.html

--- game-api/index.js
+++ game-api/index.js
@@ -32,4 +32,15 @@ app.listen(app.get('port'), () => {
   console.log(`Express web app available at localhost: ${app.get('port')}`);
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 module.exports = app;
