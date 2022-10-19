8db770a4f60700f31eb74dce552f58aad1add908
yunhl <yunhl@student.unimelb.edu.au>
2022-10-19 17:16:35 +1100

    redirect path in path resolve

--- game-api/index.js
+++ game-api/index.js
@@ -31,7 +31,7 @@ app.listen(app.get('port'), () => {
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('client/build'));
+  app.use(express.static('game-101/build'));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
