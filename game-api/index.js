063341d6c0dbf46f02bbe8ec4ac0cc3913378aff
yunhl <yunhl@student.unimelb.edu.au>
2022-10-19 17:34:42 +1100

    modified path resolve, without path.resolve in express.static

--- game-api/index.js
+++ game-api/index.js
@@ -11,8 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static(path.resolve(__dirname, '../game-101/build')));
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static(path.resolve(__dirname, '../game-101/build')));
-  // app.use(express.static('../game-101/build'));
+  // app.use(express.static(path.resolve(__dirname, '../game-101/build')));
+  app.use(express.static('../game-101/build'));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
